c366063466387a145e418b98057bb5759566692d
raisavar <raisavar@users.noreply.github.com>
2025-06-12 23:51:53 +0300

    improve: update dependencies

---
+++ src/api.js
@@ -0,0 +1,25 @@
+// API module for BarnaclePlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 9
+module.exports = API;
