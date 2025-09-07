f0529f8bac2de574be17ca9ec3394dcc56655efc
raisavar <raisavar@users.noreply.github.com>
2025-09-07 05:52:52 +0300

    refactor: code refactoring

--- src/api.js
+++ src/api.js
@@ -239,3 +239,30 @@ class API {
 
 // Update 80
 module.exports = API;
+
+
+// API module for BarnaclePlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 87
+module.exports = API;
