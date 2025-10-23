7a4898e447b7aa4fa530f8aac3cabe3b74aa9f2b
raisavar <raisavar@users.noreply.github.com>
2025-10-24 00:38:09 +0300

    docs: update README

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 64
 module.exports = API;
+
+
+// API module for BarnaclePlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 66
+module.exports = API;
