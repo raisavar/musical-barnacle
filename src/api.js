e956199780f56e558a590040d8afb0f0672b8e31
raisavar <raisavar@users.noreply.github.com>
2026-01-20 02:58:55 +0200

    feat: improve code readability

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 49
 module.exports = API;
+
+
+// API module for BarnaclePlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 64
+module.exports = API;
