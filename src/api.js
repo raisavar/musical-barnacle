421e64286b22e4ec36e28735b142ad3bc1990d2d
raisavar <raisavar@users.noreply.github.com>
2026-01-23 08:28:26 +0200

    docs(core): fix typo

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 29
 module.exports = API;
+
+
+// API module for BarnaclePlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 39
+module.exports = API;
